4f9cc22db3b53455715065a3923bf6d330c5acf1
PabloCastillo-Inetum <pablo.castillo-mesa@inetum.com>
2024-11-07 10:31:36 +0100

    execute git

--- execute-suites.sh
+++ execute-suites.sh
@@ -1,24 +1,26 @@
 # entrypoint.sh
-#!/bin/bash
+#!/bin/bash CREAR LA BUILD Y LUEGO EJECUTAR LO DE DESPUES
 
-# Ejecutar el comando Maven
-podman run  -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
-mvn clean test
+IMAGE_NAME="execution-image"
+CONTAINER_NAME="execution-container"
+GITHUB_REPO2="github.com/PabloCastillo-Inetum/DevOps_Docker.git"
+PAT="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"
 
-# Verificar que la operación anterior haya sido exitosa
-if [ $? -eq 0 ]; then
-    # Hacer copia de archivos después de los tests
-    cp <origen> <destino>
+podman build --build-arg GIT_PAT="$PAT" --build-arg GIT_REPO_URL="$GITHUB_REPO2" -t "$IMAGE_NAME" -f execution.dockerfile .
 
-    # Añadir cambios al repositorio Git
-    git add .
+podman run -d --name "$CONTAINER_NAME" "$IMAGE_NAME" mvn clean test -Dtest=TESTWEB
 
-    # Realizar un commit con un mensaje específico
-    git commit -m "Resultado de ejecución de tests"
+sleep 540
 
-    # Hacer push de los cambios al repositorio remoto
-    git push origin <nombre-branch>
-else
-    echo "Error en mvn clean test. No se ejecutarán los siguientes pasos."
-    exit 1
-fi
+podman exec "$CONTAINER_NAME" rm -rf /home/Reports/*
+podman exec "$CONTAINER_NAME" whoami
+echo "Carpeta framework:"
+podman exec "$CONTAINER_NAME" ls -l resume-report/
+echo "Carpeta git:"
+podman exec "$CONTAINER_NAME" ls -l /home/Reports/
+echo "Carpeta actual:"
+podman exec "$CONTAINER_NAME" ls -l 
+podman exec "$CONTAINER_NAME" cp -r /app/resume-report/* /home/Reports/
+podman exec "$CONTAINER_NAME" bash -c "cd /home && git add . && git commit -m 'Updated Reports' && git push"
+
+echo "Process completed successfully."
